8f5667e64e7470ea16007d5e54f28ceaf94cab83
Your Nameecho <you@example.com>
2022-12-17 15:18:19 +0530

    committed

--- company/js/company.js
+++ company/js/company.js
@@ -1,5 +1,6 @@
 /* Start Form Control Coding */
 
+let mainBox = document.querySelector("#main-box");
 let loginBox = document.querySelector(".login-box");
 let signupBox = document.querySelector(".signup-box");
 let signupBtn = document.querySelector(".signup-btn");
@@ -10,6 +11,7 @@ let loginBtn = document.querySelector(".login-btn");
 /* Start Signup Btn Coding */
 
 signupBtn.addEventListener("click", () => {
+    
     signupBox.classList.add("active");
     loginBox.classList.remove("active");
     signupBtn.classList.add("d-none");
@@ -21,6 +23,7 @@ signupBtn.addEventListener("click", () => {
 /* Start Login Btn Coding */
 
 loginBtn.addEventListener("click", () => {
+
     signupBox.classList.remove("active");
     loginBox.classList.add("active");
     signupBtn.classList.remove("d-none");
@@ -41,6 +44,7 @@ let registerBtn = document.querySelector(".register-btn");
 /* Start register btn coding */
 
 signupForm.addEventListener("submit", (e) => {
+
     e.preventDefault();
     updateRegistration();
 })
@@ -48,11 +52,13 @@ signupForm.addEventListener("submit", (e) => {
 
 
 /* Start update Registration function coding */
+
 const updateRegistration = () => {
 
     if (localStorage.getItem(signupInputEl[0].value + "_brand") == null) {
 
         if (signupInputEl[3].value.length == 10) {
+
             const userData = {
                 brandCode: signupInputEl[0].value,
                 brandName: signupInputEl[1].value,
@@ -71,12 +77,59 @@ const updateRegistration = () => {
             swal("Good job!", "Sign Up Successfully Done !", "success");
         }
         else {
-
-            swal("Contact Length Must Be 10");
+            swal("Warning !", "Contact length must be 10 !", "warning");
         }
     }
     else {
-
-        swal("This Brand Code Is Already Taken !");
+        swal("Change Brand Code !", "This Brand Code Is Already Taken !", "warning");
     }
 }
+
+
+/* Start SignIn Form Coding */
+
+let signinForm = document.querySelector(".sigin-form");
+let signinBtn = document.querySelector(".signin-btn");
+let brandCode = document.querySelector("#brand-code");
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+
+
+
+/* Start signin btn coding */
+
+signinBtn.addEventListener("click", ()=> {
+
+    if(brandCode.value && username.value && password.value != "") {
+
+        if(localStorage.getItem(brandCode.value + "_brand") != null) {
+            
+            let allData = JSON.parse(localStorage.getItem(brandCode.value + "_brand"));
+            
+            if(allData.username == username.value) {
+                
+                if(allData.password == password.value) {
+                    
+                    signinBtn.innerHTML = "Redirecting to dashboard...";
+                    signinBtn.disabled = true;
+
+                    setTimeout(() => {
+                        window.location = ("../dashboard/dashboard.html");
+                    }, 3000);
+                }
+                else {
+                    swal("Wrong password !", "Please enter registered password !", "warning");
+                }
+            }
+            else {
+                swal("Wrong username !", "Please enter registered username !", "warning");
+            }
+        }
+        else {
+            swal("Not Registered !", "Please do registration first !", "warning");
+        }
+    }
+    else {
+        swal("Input field is empty !", "Please fill all the fields !", "warning");
+    }
+})
