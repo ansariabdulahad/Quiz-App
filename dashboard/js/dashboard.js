616baa8cd23a062087d8e3fa094622e9f0206a23
Your Nameecho <you@example.com>
2022-12-22 00:23:20 +0530

    committed

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -3,7 +3,7 @@
 let profileBox = document.querySelector(".profile-box");
 let profileHeading = document.querySelector("#profile-heading");
 
-profileBox.addEventListener("click", ()=> {
+profileBox.addEventListener("click", () => {
     window.open("../images/profile.jpg");
 })
 
@@ -14,16 +14,23 @@ profileBox.addEventListener("click", ()=> {
 let brandCodeEl;
 
 // URL Security Coding.
-if(sessionStorage.getItem("brandCode") != null) {
+if (sessionStorage.getItem("brandCode") != null) {
 
     brandCodeEl = sessionStorage.getItem("brandCode");
 }
 else {
-    swal("Unauthorised Person !", "Login Again....!", "error");
+
+    document.body.innerHTML = "<h1>404 Error !" + "<br>" + "Page Not Found !</h1>";
+    document.body.style.color = "red";
+
+    setTimeout(() => {
+        swal("Unauthorised Person !", "Login Again....!", "error");
+    }, 1000);
 
     setTimeout(() => {
         window.location = "../company/company.html";
-    }, 2000);
+    }, 4000);
+
 }
 
 let brandNameEl = document.querySelector("#brand-name");
@@ -40,8 +47,8 @@ profileHeading.innerHTML = allUserData.brandName;
 
 let logoutBtn = document.querySelector("#logout-btn");
 
-logoutBtn.addEventListener("click", (e)=> {
-    
+logoutBtn.addEventListener("click", (e) => {
+
     e.target.innerHTML = "Logging Out...";
     e.target.disabled = true;
     sessionStorage.removeItem("brandCode");
@@ -52,3 +59,40 @@ logoutBtn.addEventListener("click", (e)=> {
 })
 
 /* End Logout Coding */
+
+
+
+
+/* Start section Two coding teacher box */
+
+let subjectEl = document.querySelector(".subject");
+let createSubBtn = document.querySelector(".create-subject-btn");
+
+// createSubBtn coding
+createSubBtn.onclick = function (e) {
+    e.preventDefault();
+
+    newSubject();
+}
+
+// newSubject function coding
+let visibleSubject = document.querySelector(".visible-subject");
+
+const newSubject = () => {
+
+    visibleSubject.innerHTML += `
+    
+    <div class="d-flex justify-content-between">
+        <h5>${subjectEl.value}</h5>
+
+        <div>
+            <i class="mx-2 fa-solid fa-pen-to-square"></i>
+            <i class="mx-2 fa-solid fa-floppy-disk d-none"></i>
+            <i class="mx-2 fa-solid fa-trash text-danger"></i>
+        </div>
+    </div>
+    
+    `;
+}
+
+/* End section Two coding teacher box */
